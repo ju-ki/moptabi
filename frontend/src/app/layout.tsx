2d25fde0baeaefed1bf30c3633c6f61ca51463e7
ju-ki <juki.programming@gmail.com>
2026-02-03 09:35:02 +0900

    ui系一旦コメントアウト

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,15 +3,15 @@ import './globals.css';
 
 import React from 'react';
 import { ClerkProvider } from '@clerk/nextjs';
-import { useLoadScript } from '@react-google-maps/api';
+// import { useLoadScript } from '@react-google-maps/api';
 
-import { Toaster } from '@/components/ui/toaster';
-import Header from '@/components/common/header';
+// import { Toaster } from '@/components/ui/toaster';
+// import Header from '@/components/common/header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || '',
-  });
+  // const { isLoaded } = useLoadScript({
+  //   googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || '',
+  // });
 
   return (
     <html lang="ja">
@@ -22,10 +22,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <>
           <ClerkProvider>
-            <Header />
-            {!isLoaded ? <div>Loading...</div> : <main>{children}</main>}
+            <main>{children}</main>
+            {/* <Header /> */}
+            {/* {!isLoaded ? <div>Loading...</div> : <main>{children}</main>} */}
           </ClerkProvider>
-          <Toaster />
+          {/* <Toaster /> */}
         </>
       </body>
     </html>
