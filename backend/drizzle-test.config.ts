2c3c99d3f4207f0d0ada470f5064de1ab82823dd
ju-ki <juki.programming@gmail.com>
2026-02-18 09:08:01 +0900

    prismaからdrizzleに移行

--- backend/drizzle-test.config.ts
+++ backend/drizzle-test.config.ts
@@ -1,15 +1,7 @@
-import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set');
-}
 
 export default defineConfig({
   out: './drizzle-test',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL,
-  },
 });
