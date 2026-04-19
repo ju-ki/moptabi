70c35a681c5f784d34b18547f8ffd29cb9915540
ju-ki <juki.programming@gmail.com>
2026-04-19 10:43:12 +0900

    feature202 プレビュー環境の構築

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname),
   eslint: { ignoreDuringBuilds: true },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
