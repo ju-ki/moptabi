7b262674d1153eeef66b1a8ab9e95fa0675b8959
ju-ki <juki.programming@gmail.com>
2026-07-09 23:44:06 +0900

    不要クエリ削除

--- backend/drizzle/0005_no229.sql
+++ backend/drizzle/0005_no229.sql
@@ -11,8 +11,3 @@ CREATE TABLE "PlanSpotNearestStation" (
 ALTER TABLE "PlanLocation" ADD COLUMN "time" varchar(5) NOT NULL DEFAULT '09:00';--> statement-breakpoint
 ALTER TABLE "PlanSpot" ADD COLUMN "stayDuration" integer NOT NULL DEFAULT 0;--> statement-breakpoint
 ALTER TABLE "PlanSpotNearestStation" ADD CONSTRAINT "PlanSpotNearestStation_planSpotId_fkey" FOREIGN KEY ("planSpotId") REFERENCES "public"."PlanSpot"("id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE "SpotRoute" ADD CONSTRAINT "SpotRoute_planId_fkey" FOREIGN KEY ("planId") REFERENCES "public"."Plan"("id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE "SpotRoute" ADD CONSTRAINT "SpotRoute_fromPlanSpotId_fkey" FOREIGN KEY ("fromPlanSpotId") REFERENCES "public"."PlanSpot"("id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE "SpotRoute" ADD CONSTRAINT "SpotRoute_toPlanSpotId_fkey" FOREIGN KEY ("toPlanSpotId") REFERENCES "public"."PlanSpot"("id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE "SpotRoute" ADD CONSTRAINT "SpotRoute_fromNearestStationId_fkey" FOREIGN KEY ("fromNearestStationId") REFERENCES "public"."PlanSpotNearestStation"("id") ON DELETE set null ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE "SpotRoute" ADD CONSTRAINT "SpotRoute_toNearestStationId_fkey" FOREIGN KEY ("toNearestStationId") REFERENCES "public"."PlanSpotNearestStation"("id") ON DELETE set null ON UPDATE cascade;
